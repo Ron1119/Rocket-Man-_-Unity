0115ac08b028e90c46ae8adeaa2453600efc5880
Ron9 <lvronghui19@gmail.com>
2019-06-18 18:13:56 -0400

    Add sine wave move to object,scipts were removed from prefab

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -7,8 +7,10 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
 
-    [SerializeField] Vector3 movementVector;
-    [Range(0,1)] [SerializeField] float movementFactor;
+    [SerializeField] Vector3 movementVector = new Vector3 (20f, 0f, 0f);
+
+    [SerializeField] float period = 2f;
+    float movementFactor;
 
     Vector3 startPos;
     // Start is called before the first frame update
@@ -21,6 +23,10 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau); // -1 to 1
+        movementFactor = rawSinWave/2 + 0.5f; // 0 to 1
         Vector3 offset = movementVector * movementFactor;
         transform.position = startPos + offset;
     }

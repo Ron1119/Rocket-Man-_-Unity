84b25719b47b1d6f6bbeea9d68bdf12da19d5c25
Ron9 <lvronghui19@gmail.com>
2019-06-18 17:38:52 -0400

    attach the oscillator to the object 9, and the movement format

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -3,23 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
+[DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
 
     [SerializeField] Vector3 movementVector;
-    [Range(0, 1)] [SerializeField] float movementFactor;
+    [Range(0,1)] [SerializeField] float movementFactor;
 
     Vector3 startPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;  // get the initial position
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startPos + offset;
     }
 }
